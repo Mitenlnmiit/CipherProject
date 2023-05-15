e95ff8bf48eba5cb38005aea5082dbccec9769a9
Mitenlnmiit <122150398+Mitenlnmiit@users.noreply.github.com>
2023-05-15 11:10:54 +0530

    Add files via upload

---
+++ contract/BuyMeACoffee.sol
@@ -0,0 +1,69 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract BuyCoffee {
+    // Event to emit when a Memo is created.
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+    
+    // Memo struct.
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+    
+    
+    address payable owner;
+
+    Memo[] memos;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev fetches all stored memos
+     */
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
+
+    /**
+     * @dev buy a coffee for owner (sends an ETH tip and leaves a memo)
+     * @param _name name of the coffee purchaser
+     * @param _message a nice message from the purchaser
+     */
+    function buyCoffee(string memory _name, string memory _message) public payable {
+        // accept only when giving some ether , i.e greater than zero 
+        require(msg.value > 0, "can't buy coffee for free!");
+
+        // Add the memo to storage!
+        memos.push(Memo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        ));
+
+        // Emit a NewMemo event with details about the memo.
+        emit NewMemo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+
+    /**
+     * @dev send the entire balance stored in this contract to the owner
+     */
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+}
